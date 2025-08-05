53b83eb95ef0f399607df96ca32b6e0b34190026
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-08-05 11:41:50 -0300

    dadad

---
+++ pratica_vue/ex001/src/App.vue
@@ -0,0 +1,104 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import teste from './components/teste.vue'
+import {ref} from "vue";
+
+var hidden = false;
+  const array = ref([
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "delectus aut autem",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "quis ut nam facilis et officia qui",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "fugiat veniam minus",
+      "completed": false
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "et porro tempora",
+      "completed": true
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+      "completed": false
+    }
+  ])
+
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+<<<<<<< HEAD
+      <p> z</p>
+=======
+
+
+    </div>
+    <teste > </teste>
+  </header>
+
+
+  <div v-show="hidden" v-for="obj in array" v-bind:key="obj.id" class="teste" >
+
+    {{obj.title}}
+  </div>
+
+  <main>
+    <TheWelcome />
+  </main>
+
+
+
+
+</template>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+.teste{
+
+}
+</style>
