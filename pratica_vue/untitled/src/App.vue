6ecc3d3abbfed74ecc1633edc2be4c05ac1cc01d
NandoJunioDev <frnndmaz_cf@outlook.com>
2025-08-04 21:27:19 -0300

    dadad

---
+++ pratica_vue/untitled/src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+import Cabecalho from "@/components/Cabecalho.vue";
+import ListaCidades from "@/components/ListaCidades.vue";
+import {ref} from "vue";
+</script>
+
+<template>
+  <cabecalho/>
+  <div class="lista">
+    <div class="card" v-for="cidade in cidades " :key="cidade.id" >
+    <div class="card-imagem"></div>
+      <h3>{{cidade.name}}</h3>
+      <h4>{{cidade.description}}</h4>
+      <div class="temperatura ">{{}}º</div>
+
+
+
+    </div>
+
+  </div>
+
+<lista-cidades/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ pratica_vue/untitled/src/components/ListaCidades.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {ref} from "vue";
+  let testa = ref()
+</script>
+
+<template>
+
+  <label for="testa"> Selecione  as cidades em baixo</label>
+       <select v-model="testa">
+         <option value="">Selecione</option>
+         <option value="rioDeJaneiro">Rio de Janeiro</option>
+         <option value="saoPaulo"> Sao Paulo</option>
+         <option value="minasGerais">Minas Gerais</option>
+         
+       </select>
+  
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
