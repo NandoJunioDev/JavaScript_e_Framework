122f7456536e5bc46e1f21561591e4430facc806
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-08-06 17:41:59 -0300

    dadadadad

--- pratica_vue/untitled/src/components/ListaCidades.vue
+++ pratica_vue/untitled/src/components/ListaCidades.vue
@@ -20,7 +20,7 @@ const KEY = "97297449decc435ba1101111250608"; // chave de acesso da API
         throw  new Error(retorno.error?.message|| 'erro');
       }
 
-      mostrarPrevisao(retorno)
+
       //celcius_c.value = Math.round(retorno.current.temp_c)// aqui recembos valor da api, currente,temp_c , sao dados da api em json
     }
 
@@ -32,22 +32,7 @@ const KEY = "97297449decc435ba1101111250608"; // chave de acesso da API
 
   }
 
-function mostrarPrevisao(retorno) {
-  const location = retorno.location;
-  const current = retorno.current;
-  const forecast = retorno.forecast;
-  const location_name = location.name;
-  const country = location.country;
-  const temp_c = current.temp_c;
-  const temp_f = ref(current.temp_f);
-  const condition = current.condition;
-  const wind_kph = current.wind_kph;
-  const wind_mph = current.wind_mph;
-  const wind_degree = current.wind_degree;
 
-
-
-let celcius_c = Math.round(temp_c)  ;
 
 
 }
